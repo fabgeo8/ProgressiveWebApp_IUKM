03abc072695f6079adc3efc7e0a7e1fc38007491
cedricrettus <cedric.sutter@ntb.ch>
2017-11-23 16:20:03 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,39 +1,47 @@
 
-var cacheName = 'WebApp';
+const cacheName = 'WebApp';
+const offlineUrl = 'offlinePage.html';
+
 // Cache our known resources during install
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(cacheName)
-.then(cache => cache.addAll([ 
-'./home.js',
-'./style.css',
-'./index.html'
-]))
-);
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([ 
+        './home.js',
+        './style.css',
+        './index.html',
+        './camping.png',
+        './camping2.png',
+        './camping2.png',
+        './webAPP.json',
+        offlineUrl
+      ]))
+    );
 });
-// Cache any new resources as they are fetched
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request, { ignoreSearch: true })
-.then(function(response) {
-if (response) {
-return response;
-}
-var requestToCache = event.request.clone();
-return fetch(requestToCache).then( 
-function(response) {
-if(!response || response.status !== 200) {
-return response;
-}
-var responseToCache = response.clone();
-caches.open(cacheName)
-.then(function(cache) {
-cache.put(requestToCache, responseToCache);
-});
-return response;
-});
-})
-);
-});
+self.addEventListener('fetch', event => { 
+  
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      return response;
+    }
+    var fetchRequest = event.request.clone();
+    
+    return fetch(fetchRequest).then(function (response) {
+      if (!response || response.status !== 200) {
+      return response;
+      }
+      var responseToCache = response.clone();
+      caches.open(cacheName).then(function (cache) {
+      cache.put(event.request, responseToCache);
+      });
+      
+      return response;
+  }).catch(error => { 
+      if (event.request.method === 'GET' &&
+        event.request.headers.get('accept').includes('text/html')) {
+          return caches.match(offlineUrl); 
+      }
+  });
+}));
 
 
