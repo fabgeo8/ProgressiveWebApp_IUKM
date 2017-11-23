c60e68bf3d5c507a9bc10914bc93a0a6fc26b609
cedricrettus <cedric.sutter@ntb.ch>
2017-11-23 11:39:22 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 this.addEventListener('fetch', function (event) {
 if(event.request.headers.get('save-data')){
 // We want to save data, so restrict icons and fonts
-if (event.request.url.includes('fonts.googleapis.com')) {
+if (event.request.url.includes('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')) {
 // return nothing
 event.respondWith(new Response('', {status: 417, statusText: 'Ignore fonts to save data.' }));
 }
