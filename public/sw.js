954eaac9160f0cad5f41d7955292d0940b3215ee
cedricrettus <cedric.sutter@ntb.ch>
2017-11-23 11:09:47 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -35,13 +35,4 @@ return response;
 );
 });
 
-"use strict";
-this.addEventListener('fetch', function (event) {
-if(event.request.headers.get('save-data')){
-// We want to save data, so restrict icons and fonts
-if (event.request.url.includes('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')) {
-// return nothing
-event.respondWith(new Response('', {status: 417, statusText: 'Ignore fonts to save data.' }));
-}
-}
-});
+
