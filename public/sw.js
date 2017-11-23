62888b69a568454a7cac3dec169d7c0f40d8386d
cedricrettus <cedric.sutter@ntb.ch>
2017-11-23 11:34:20 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,14 @@
-
+this.addEventListener('fetch', function (event) {
+if(event.request.headers.get('save-data')){
+  console.log("gagi");
+// We want to save data, so restrict icons and fonts
+if (event.request.url.includes('fonts.googleapis.com')) {
+// return nothing
+event.respondWith(new Response('', {status: 417, statusText: 'Ignore fonts to save
+data.' }));
+}
+}
+});
 var cacheName = 'WebApp';
 // Cache our known resources during install
 self.addEventListener('install', event => {
