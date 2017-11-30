fd8dfac31c30471a166e275a853ae589ffc9f5b2
cedricrettus <cedric.sutter@ntb.ch>
2017-11-30 17:09:49 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,13 +2,13 @@ importScripts('./idb-keyval.js');
 self.addEventListener('sync', (event) => { 
 if (event.tag === 'notizen') { 
 event.waitUntil(
-idbKeyval.get('sendMessage').then(value => 
+idbKeyval.get('todo').then(value => 
 fetch('/sendMessage/', { 
 method: 'POST',
 headers: new Headers({ 'content-type': 'application/json' }),
 body: JSON.stringify(value) 
 })));
-idbKeyval.delete('sendMessage'); 
+idbKeyval.delete('todo'); 
 }
 });
 
