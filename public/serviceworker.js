630de2699191763b51b7fa142673a7aa3b88836e
cedricrettus <cedric.sutter@ntb.ch>
2017-11-30 16:29:56 +0100

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,3 +1,30 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js');
-}
+
+if ('serviceWorker' in navigator && 'SyncManager' in window) { ❶
+navigator.serviceWorker.register('./sw.js')
+.then(registration => navigator.serviceWorker.ready)
+.then(registration => {
+document.getElementById('submit').addEventListener('click', () => {
+registration.sync.register('notizen').then(() => {
+var payload = {
+notiz: document.getElementById('text').value,
+
+};
+idbKeyval.set('sendMessage', payload);
+});
+});
+});
+} else {
+document.getElementById('submit').addEventListener('click', () => { ❷
+var payload = { ❸
+notiz: document.getElementById('text').value,
+
+};
+fetch('/sendMessage/', ❹
+{
+method: ‘POST’,
+headers: new Headers({
+'content-type': 'application/json'
+}),
+body: JSON.stringify(payload)
+});
+});
