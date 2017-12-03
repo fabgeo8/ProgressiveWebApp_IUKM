8ea316c80db0c786fbe6e6cff7aa5bff1f836f34
cedricrettus <cedric.sutter@ntb.ch>
2017-12-03 10:46:44 +0100

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator && 'SyncManager' in window) { 
 navigator.serviceWorker.register('./sw.js')
 .then(registration => navigator.serviceWorker.ready)
-  /*.then(registration => {
+.then(registration => {
     document.getElementById('newtodo').addEventListener('click', () => {
       registration.sync.register('notizen').then(() => {
         var payload = {
@@ -11,7 +11,7 @@ navigator.serviceWorker.register('./sw.js')
       idbKeyval.set('todo', payload);
       });
     });
-}); */
+});
 } else {
 document.getElementById('submit').addEventListener('click', () => { 
 var payload = { 
