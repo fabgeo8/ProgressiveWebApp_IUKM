343d3f0004b99e12247aa60e21f9b37a52d29f91
cedricrettus <cedric.sutter@ntb.ch>
2017-12-12 09:50:01 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -43,17 +43,4 @@ self.addEventListener('fetch', function(event) {
 
 
 
-/*importScripts('./idb-keyval.js');
-self.addEventListener('sync', (event) => { 
-if (event.tag === 'notizen') { 
-event.waitUntil(
-idbKeyval.get('todo').then(value => 
-fetch('/todo', { 
-method: 'POST',
-headers: new Headers({ 'content-type': 'application/json' }),
-body: JSON.stringify(value) 
-})));
-idbKeyval.delete('todo'); 
-}
-});
 
