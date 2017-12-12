74fe671c559828e103711ffcd5ccd362c9315a21
cedricrettus <cedric.sutter@ntb.ch>
2017-12-12 09:45:33 +0100

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,35 +1,8 @@
 
-if ('serviceWorker' in navigator && 'SyncManager' in window) { 
-    
-if  (navigator.onLine){
-navigator.serviceWorker.register('./sw.js')
-console.log('serviceWorker regiistriert!')
-    .then(registration => navigator.serviceWorker.ready)
-
-    .then(registration => {
-
-        document.getElementById('newtodo').addEventListener('click', () => {
-          registration.sync.register('notizen').then(() => {
-            var payload = {
-              text: document.getElementById('text').value,
-            };
-          idbKeyval.set('todo', payload);
-          });
-        });
-      });
-} else {
-/*document.getElementById('submit').addEventListener('click', () => { 
-var payload = { 
-text: document.getElementById('text').value,
-
-};
-fetch('/todo', 
-{
-method: 'POST',
-headers: new Headers({
-'content-type': 'application/json'
-}),
-body: JSON.stringify(payload)
-});
-});*/
-}
+if ('serviceWorker' in navigator) { 
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function(err) { 
+                console.log('ServiceWorker registration failed: ', err);
+    });
+} 
