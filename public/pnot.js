488a2b03a443cf32d3cc3f49e11e87461bf5b574
cedricrettus <cedric.sutter@ntb.ch>
2017-12-14 16:14:01 +0100

    Update pnot.js

--- public/pnot.js
+++ public/pnot.js
@@ -48,6 +48,12 @@ function initializeUI() {
 }
 
 function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
  if (isSubscribed) {
     pushButton.textContent = 'Disable Push Messaging';
   } else {
