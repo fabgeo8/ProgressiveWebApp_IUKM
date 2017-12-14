db88daffbd3e1dbbcb489b62e3cc2baa4ba67e47
cedricrettus <cedric.sutter@ntb.ch>
2017-12-14 16:18:56 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -57,6 +57,19 @@ self.addEventListener('sync', (event) => {
          idbKeyval.delete('todo'); 
       }
 });
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
 
 
 
