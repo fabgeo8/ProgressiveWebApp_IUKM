29cfcf22b14c4a6ef55a6130bb68146b43871280
cedricrettus <cedric.sutter@ntb.ch>
2017-12-14 09:37:33 +0100

    Update pnot.js

--- public/pnot.js
+++ public/pnot.js
@@ -23,11 +23,18 @@ function urlB64ToUint8Array(base64String) {
 }
 
 function initializeUI() {
-  // Set the initial subscription value
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
   swRegistration.pushManager.getSubscription()
   .then(function(subscription) {
     isSubscribed = !(subscription === null);
-
+  updateSubscriptionOnServer(subscription);
    if (isSubscribed) {
       console.log('User IS subscribed.');
     } else {
@@ -47,3 +54,39 @@ function updateBtn() {
 
   pushButton.disabled = false;
 }
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
