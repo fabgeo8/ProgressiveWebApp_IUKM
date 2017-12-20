3205681f85abd777cfa4d077435c11085228923d
cedricrettus <cedric.sutter@ntb.ch>
2017-12-20 10:35:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var app = express();
 
+const webpush = require('web-push'); 
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+webpush.setVapidDetails( 
+ 'mailto:contact@deanhume.com',
+'BAyb_WgaR0L0pODaR7wWkxJi__tWbM1MPBymyRDFEGjtDCWeRYS9EF7yGoCHLdHJi6hikYdg4MuYaK0XoD0qnoY',
+ 'p6YVD7t8HkABoez1CvVJ5bl7BnEdKUu5bSyVjyxMBh0'
+);
+app.post('/pushNot', function (req, res) { 
+  var endpoint = req.body.endpoint;
+saveRegistrationDetails(endpoint, key, authSecret); 
+ const pushSubscription = { 
+  endpoint: req.body.endpoint,
+    keys: {
+      auth: req.body.authSecret,
+      p256dh: req.body.key
+      }
+ };
+ var body = 'Thank you for registering';
+ var iconUrl = 'https://example.com/images/homescreen.png';
+ webpush.sendNotification(pushSubscription, 
+  JSON.stringify({
+    msg: body,
+    url: 'http://localhost:3111/',
+    icon: iconUrl
+ }))
+ .then(result => res.sendStatus(201))
+ .catch(err => { console.log(err); });
+});
+app.listen(3111, function () {
+ console.log('Web push app listening on port 3111!')
+});
+
 app.use(express.static('public'));
 
 
