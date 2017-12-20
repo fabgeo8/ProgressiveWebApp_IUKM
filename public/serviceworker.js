786ceb968b96e421779d2de504c0eb27000be7d5
cedricrettus <cedric.sutter@ntb.ch>
2017-12-20 11:42:25 +0100

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
      navigator.serviceWorker.register('/sw.js')
      .then(registration => navigator.serviceWorker.ready) 
      .then(registration => {
-         document.getElementById('text').addEventListener('click', () => {          
+         document.getElementById('submit').addEventListener('click', () => {          
              registration.sync.register('textNachricht').then(() => { 
      var payload = {
          text: document.getElementById('text').value,
