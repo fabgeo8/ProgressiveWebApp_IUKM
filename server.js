68f4aa0fdc76edf2d4fc316cc2375b14f19bea8c
cedricrettus <cedric.sutter@ntb.ch>
2017-12-21 14:54:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.json());
 
 
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
